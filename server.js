c646ed7763b1bffd352422bfc2d29f913f72c98a
arvind jha <arvind.ubarri@gmail.com>
2024-03-22 16:37:57 +0530

    changes in server js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from "path";
 import { fileURLToPath } from "url";
+
 //configure env
 dotenv.config();
 
@@ -17,13 +19,6 @@ connectDB();
 //esmodule fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-//middelwares
-app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
-
 //rest object
 const app = express();
 
@@ -31,6 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
