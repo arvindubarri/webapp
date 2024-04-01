f237e7415f86195a3a922d31002eef8e833dcdfb
arvind jha <arvind.ubarri@gmail.com>
2024-04-01 22:12:11 +0530

    changes on 1st aprill4 multer

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,16 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+  uplodController,
+  recivedQueryController,
+  queryController,
+} from "../controller/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import multer from "multer";
 
-// const upload = multer({ dest: "uploads/" });
 // const upload = multer({ dest: "uploads/" });
 
 const storage = multer.memoryStorage();
@@ -10,29 +19,33 @@ const upload = multer({ storage: storage });
 //router object
 const router = express.Router();
 
-
-
 //routing
 //REGISTER || METHOD POST
 router.post("/register", registerController);
+
 //LOGIN || POST
 router.post("/login", loginController);
+
 //LOGIN || POST
 router.post("/forgot-password", forgotPasswordController);
+
 // contact us query
 router.post("/contactus", queryController);
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
+
 //test routes
 //protected  User route auth
 router.get("/user-auth", requireSignIn, (req, res) => {
   res.status(200).send({ ok: true });
 });
+
 //protected Admin route auth
 router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
 // router.get("/allvoters",requireSignIn, )
+
 router.post(
   "/uploads",
   requireSignIn,
@@ -43,6 +56,8 @@ router.post(
     res.status(200).send({ ok: true });
   }
 );
+
 // all queries
 router.get("/queries", requireSignIn, isAdmin, recivedQueryController);
+
 export default router;
